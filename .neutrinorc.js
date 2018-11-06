c227f9d21ec603418d953dbd6798ac04fdd41ef8
Ken Wu <ken.wu@ibm.com>
2018-11-05 22:26:30 -0500

    first

---
+++ .neutrinorc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  use: [
+    '@neutrinojs/airbnb',
+    [
+      '@neutrinojs/react',
+      {
+        html: {
+          title: 'n-react1'
+        }
+      }
+    ],
+    '@neutrinojs/jest'
+  ]
+};
